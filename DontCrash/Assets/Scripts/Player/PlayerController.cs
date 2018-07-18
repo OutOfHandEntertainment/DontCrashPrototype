971d3a7737aedba17668760d7950b6879a897a1a
Niralius <michaelheffnercruz@gmail.com>
2018-07-18 09:46:24 -0400

    Preliminary prototype

--- DontCrash/Assets/Scripts/Player/PlayerController.cs
+++ DontCrash/Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
 }
